80b292a5a7a64edf1cc98df4b56d797fca744bd2
JonnyDem <tl-k@ukr.net>
2017-09-06 13:08:03 +0300

    Initial commit

---
+++ src/main/java/fromscannertofile/EnterFromScanner.java
@@ -0,0 +1,39 @@
+package fromscannertofile;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by Ваня on 06.09.2017.
+ */
+public class EnterFromScanner {
+
+    public static void main(String[] args) {
+
+        writeTextFromScannerToFile();
+    }
+    
+    private static void writeTextFromScannerToFile()  {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter path where you want to create new or use existing file (example: C:\\Users\\IdeaProjects\\fromscannertofile\\src\\main\\resources\\Scanner.txt)");
+        String path = scan.nextLine();
+        File file = new File(path);
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            System.out.println("Enter text");
+            String text = scan.nextLine();
+            FileWriter fw = new FileWriter(file, true);
+            fw.write(text + "\n");
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            scan.close();
+        }
+    }
+}
